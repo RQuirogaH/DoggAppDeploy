77edec4b795900a7f7698dffac84cc3c05390674
RQuirogaH <rafarqh@gmail.com>
2022-07-03 01:38:16 -0500

    form design v1

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -4,6 +4,7 @@ import s from './Form.module.css'
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTemperaments } from "../../Redux/actions";
+import Card from "../Card/Card";
 
 
 const Form = () => {
@@ -76,69 +77,76 @@ const Form = () => {
 
     return (
         <div className={s.container}>
-            <h3>Create your dog</h3>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <label htmlFor='name'>Name</label>
-                <input type="text" id="name" onChange={(e) => handleChangeText(e)} value={input.name} />
-                <br />
+            <section>
+                <form onSubmit={(e) => handleSubmit(e)} className={s.formulario}>
+                    <h3>Create your dog</h3>
 
-                <label htmlFor='height_min' className={s.height}>Height [cm]
-                    <label htmlFor='height_min'> min:</label>
-                    <input type='text' id="height_min" onChange={(e) => handleChangeNumber(e)} value={input.height_min} />
-                    <label htmlFor='height_max'>max</label>
-                    <input type='text' id="height_max" onChange={(e) => handleChangeNumber(e)} value={input.height_max} />
-                </label>
+                    <label htmlFor='name'>Name</label>
+                    <div>
+                        <input type="text" id="name" onChange={(e) => handleChangeText(e)} value={input.name} autoComplete='off' />
+                    </div>
 
-                <br />
+                    <label htmlFor='height_min' className={s.minMax}>Height</label>
+                    <div>
+                        <input type='text' id="height_min" onChange={(e) => handleChangeNumber(e)} value={input.height_min} autoComplete='off' placeholder="min (required)" />
+                        <label htmlFor='height_max'> - </label>
+                        <input type='text' id="height_max" onChange={(e) => handleChangeNumber(e)} value={input.height_max} autoComplete='off' placeholder="max (optional)" />
+                        <label htmlFor='height_min'> cm</label>
+                    </div>
 
-                <label htmlFor='weight_min' className={s.weight}>Weight [kg]
-                    <label htmlFor='weight_min'> min:</label>
-                    <input type='text' id="weight_min" onChange={(e) => handleChangeNumber(e)} value={input.weight_min} />
-                    <label htmlFor='weight_max'>max</label>
-                    <input type='text' id="weight_max" onChange={(e) => handleChangeNumber(e)} value={input.weight_max} />
-                </label>
+                    <label htmlFor='weight_min' className={s.minMax}>Weight</label>
+                    <div>
+                        <input type='text' id="weight_min" onChange={(e) => handleChangeNumber(e)} value={input.weight_min} autoComplete='off' placeholder="min (required)" />
+                        <label htmlFor='weight_max'> - </label>
+                        <input type='text' id="weight_max" onChange={(e) => handleChangeNumber(e)} value={input.weight_max} autoComplete='off' placeholder="max (optional)" />
+                        <label htmlFor='weight_min'> kg</label>
+                    </div>
 
-                <br />
+                    <label htmlFor='life_min' className={s.minMax}>Life span</label>
+                    <div>
+                        <input type="text" id="life_min" onChange={(e) => handleChangeNumber(e)} value={input.life_min} autoComplete='off' placeholder="min (required)" />
+                        <label htmlFor='leight_max'> - </label>
+                        <input type="text" id="life_max" onChange={(e) => handleChangeNumber(e)} value={input.life_max} autoComplete='off' placeholder="max (optional)" />
+                        <label htmlFor='leight_max'> years </label>
+                    </div>
 
-                <label htmlFor='life_min' className={s.life}>Life span
-                    <label htmlFor='life_min'> min:</label>
-                    <input type="text" id="life_min" onChange={(e) => handleChangeNumber(e)} value={input.life_min} />
-                    <label htmlFor='leight_max'>max</label>
-                    <input type="text" id="life_max" onChange={(e) => handleChangeNumber(e)} value={input.life_max} />
-                </label>
+                    <label htmlFor='url'>Image URL</label>
+                    <div>
+                        <input type="text" id="url" onChange={(e) => handleChange(e)} value={input.url} autoComplete='off' />
+                    </div>
 
-                <br />
+                    <label htmlFor="temperaments">Temperaments:</label>
+                    <div>
+                        <select defaultValue='Select_temperaments' name="temperaments" id="temperaments" onChange={(e) => handleSetTemperaments(e)}>
+                            <option disabled value="Select_temperaments">Select temperaments</option>
+                            {
+                                temperaments.map(e => {
+                                    return <option key={e} value={`${e}`}>{e}</option>
+                                })
+                            }
+                        </select>
+                    </div>
 
-                <label htmlFor='url'>Image URL</label>
-                <input type="text" id="url" onChange={(e) => handleChange(e)} value={input.url} />
+                    <div className={s.temperaments}>
+                        {
+                            input.temperaments?.map((t, ind) => <div key={`temp${ind}`}>
+                                <span>{t}</span>
+                                <button type="button" value={t} onClick={(e) => handleDeleteTemp(e)}>X</button>
+                            </div>)
+                        }
+                    </div>
 
-                <br />
-
-                <label htmlFor="temperaments">Temperaments: </label>
-                <select defaultValue='Select_temperaments' name="temperaments" id="temperaments" onChange={(e) => handleSetTemperaments(e)}>
-                    <option disabled value="Select_temperaments">Select temperaments</option>
-                    {
-                        temperaments.map(e => {
-                            return <option key={e} value={`${e}`}>{e}</option>
-                        })
-                    }
-                </select>
-
-                <br />
-
-                <div className={s.temperaments}>
-                    {
-                        input.temperaments?.map((t, ind) => <div key={`temp${ind}`}>
-                            <span>{t}</span>
-                            <button type="button" value={t} onClick={(e) => handleDeleteTemp(e)}>X</button>
-                        </div>)
-                    }
+                    <button type="submit">Enviar</button>
+                </form>
+                <div className={s.card}>
+                    <Card
+                        key={`${input.name}_${input.url}`}
+                        name={input.name}
+                        weight={input.weight}
+                        temperament={input.temperaments}
+                        img={input.url} />
                 </div>
-
-                <br />
-
-                <button type="submit">Enviar</button>
-            </form>
+            </section>
         </div>
     )
 }
